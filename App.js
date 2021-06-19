15be0b82510aed20ec41b83de963b8f4dc58d40d
Alberto626 <47710569+Alberto626@users.noreply.github.com>
2021-06-18 20:50:38 -0700

    my work

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {HomeScreenStack} from './Navigator/StackNavigation'
+
+const App= props => {
+  return (
+    <NavigationContainer>
+      <HomeScreenStack/>
+    </NavigationContainer>
+  );
+}
+
+export default App;
